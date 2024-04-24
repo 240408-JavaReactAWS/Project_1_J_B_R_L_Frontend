6e63ef29fc8c6717ac005d07633ed4b73fcbbdc2
rdsherk <ryan.sherk@gmail.com>
2024-04-24 16:14:50 -0400

    Added MovieContainer that displays a list of  a users movies

---
+++ project_1_j_b_r_l_frontend/src/movie/Movie.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { IMovie } from "../models/IMovie";
+import "./Movie.css";
+
+interface IMovieProps {
+    movie: IMovie;
+}
+
+function Movie(props: IMovieProps) {
+    return (
+        <div className="movie">
+            <h2>{props.movie.name}</h2>
+            <p>Price: {props.movie.price}</p>
+        </div>
+    )
+}
+
+export default Movie;
