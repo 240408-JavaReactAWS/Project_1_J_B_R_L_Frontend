cf61edc58d49d8843f306d8f56643a5797f57822
briandb3 <75955003+briandb3@users.noreply.github.com>
2024-04-29 16:47:08 -0400

    Fix for redirects when user is not logged in, added delete button to the list of movies which appears only if the user is an admin

--- project_1_j_b_r_l_frontend/src/components/userMovies.tsx
+++ project_1_j_b_r_l_frontend/src/components/userMovies.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import DeleteButton from './deletebutton/DeleteButton';
 
 interface Movie {
     movieId: number;
@@ -28,6 +29,43 @@ const UserMovies: React.FC = () => {
         }
     };
 
+    let navigateTo = useNavigate();
+
+    useEffect(() => {
+        let checkForLoggedInUser = async () => {
+            try {
+                let res = await axios.get('http://localhost:8080/users/session', {
+                    withCredentials: true
+                });
+            } catch (error : any) {
+                let status = error.response.status;
+                if (status === 401) {
+                    console.log("Session is invalid");
+                    navigateTo('/users/login');
+                } else if (status === 403) {
+                    console.log("Unauthorized access");
+                    navigateTo('/users/login');
+                }
+            }
+        }
+
+        let checkForAdminUser = async () => {
+            try {
+                let res = await axios.get('http://localhost:8080/users/admin', {
+                    withCredentials: true
+                });
+                isAdmin = true;
+            } catch (error : any) {
+                isAdmin = false;
+            }
+        }
+    
+        checkForAdminUser();
+    
+        checkForLoggedInUser();
+    }, []);
+    let isAdmin = localStorage.getItem("admin") === "true";
+
     return (
         <div>
             <h1>Movie List</h1>
@@ -50,6 +88,7 @@ const UserMovies: React.FC = () => {
 
                             <td>{movie.description}</td>
                             <td><img src={movie.url} alt="movie poster" width="100" height="150" /></td>
+                            <td><DeleteButton id={movie.movieId} isAdmin={isAdmin} setMovies={setMovies}/></td>
                         </tr>
                     ))}
                 </tbody>
