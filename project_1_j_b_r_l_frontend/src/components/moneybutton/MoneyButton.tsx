3971a4f44a6d3b2dfada58329dea17488471c409
briandb3 <75955003+briandb3@users.noreply.github.com>
2024-04-30 16:04:35 -0400

    Added amount of money in account to MoneyButton component

--- project_1_j_b_r_l_frontend/src/components/moneybutton/MoneyButton.tsx
+++ project_1_j_b_r_l_frontend/src/components/moneybutton/MoneyButton.tsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import "./MoneyButton.css";
-import React, { SyntheticEvent, useState } from 'react';
+import React, { SyntheticEvent, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function MoneyButton(){
     const [amount, setAmount] = useState(0);
     const [text, setText] = useState('');
     const [className, setClassName] = useState('');
+    const [balance, setBalance] = useState(0);
 
     let updateAmount = (e: SyntheticEvent) => {
         setAmount(parseFloat((e.target as HTMLInputElement).value))
@@ -28,6 +30,7 @@ function MoneyButton(){
                     // Handle successful response
                     console.log('PATCH request sent successfully');
                     setClassName('success');
+                    setBalance(balance + amount);
                     setText('$' + amount.toFixed(2) + ' added to your account');
                 } else {
                     // Handle error response
@@ -44,10 +47,31 @@ function MoneyButton(){
         }
         setAmount(0);
 };
+    let navigateTo = useNavigate();
+    useEffect(() => {
+        
+        let checkForLoggedInUser = async () => {
+            try {
+                let res = await axios.get('http://localhost:8080/users/session', {
+                    withCredentials: true
+                });
+                setBalance(res.data.balance);
+            } catch (error : any) {
+                let status = error.response.status;
+                if (status === 401) {
+                    console.log("Session is invalid");
+                    navigateTo('/users/login');
+                }
+            }
+            console.log("hello world")
+        }
+    
+        checkForLoggedInUser();
+    }, [])
 
     return (
         <div className='movie-form'>
-            <label htmlFor="amount" className ="movie-label">Amount:</label>
+            <label htmlFor="amount" className ="movie-label">Amount: {balance}</label>
             <input onChange={updateAmount} value={amount} type="number" id="amount" name="amount"/>
             <button onClick={handleButtonClick}>
                 Add Money
